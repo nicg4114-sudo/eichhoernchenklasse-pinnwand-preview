108b965a53ed3343ab767bac7afdb3ca23a8ccbe
nicg4114-sudo <nicg4114@gmail.com>
2026-08-05 16:07:45 +0200

    Add VAPID_PUBLIC_KEY to configuration

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.PINNWAND_CONFIG = {
     SUPABASE_URL: "https://zproqvwybbhsvlpyfowg.supabase.co",
     SUPABASE_ANON_KEY: "sb_publishable_84_Pm_IGOKbknEXXKP6IgQ_YtPdxNHa",
+    VAPID_PUBLIC_KEY: "BBaVziOX6RTQDuRR7XXp8bJ62sLppVCotJiC3kvH9yCFO-aUgB71rVQGdRFBS91hkpgZB6Z-7Xnv4D7gw9mbyuw",
   };
