8b2a9a2202ae2385e1f0ba491238f9d3ddccd649
nicg4 <nicg4114@gmail.com>
2026-09-04 11:09:52 +0200

    Bugfix: Monats-Pfeile im Kalender unsichtbar (0x0 Pixel)

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 // Anfragen an Supabase (die eigentlichen Pinnwand-Daten) laufen immer direkt
 // übers Netz, damit nie veraltete Inhalte angezeigt werden.
 
-const CACHE_NAME = "pinnwand-shell-v30";
+const CACHE_NAME = "pinnwand-shell-v31";
 const SHELL_FILES = [
   "./",
   "./index.html",
